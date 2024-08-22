eb440dd3e9fe23345792abb1f9b40ea6bf078a07
Dines Dayrit <84615498+dinesdayrit@users.noreply.github.com>
2024-08-22 13:49:21 +0800

    feat: add our process section

--- js/main.js
+++ js/main.js
@@ -9,6 +9,7 @@ $(document).ready(function () {
   $("#success-stories-section").load("html/successStories.html");
   $("#tech-partners-section").load("html/partners.html");
   $("#email-sms-section").load("html/emailAndSms.html");
+  $("#our-process-section").load("html/process.html");
   // Function to initialize the card group
   function initializeCards() {
     const cardData = [
